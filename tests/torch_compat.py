c6480db2d16c86802a78c8ca146fff45367e4481
mpariente <pariente.mnl@gmail.com>
2020-11-21 21:17:02 +0100

    ISTFT also passes with square window, in the fully overlapping parts. More work to do for the w**2 OLA correction, the first and last frames. Starting to look good.

--- tests/torch_compat.py
+++ tests/torch_compat.py
@@ -31,51 +31,60 @@ class TorchSTFT(STFTFB):
         spec[..., self.n_filters // 2, :] *= np.sqrt(2)
         return spec * (self.kernel_size // 2) ** 0.5
 
-    # def pre_synthesis(self, spec):
-    #     spec[..., 0, :] /= np.sqrt(2)
-    #     spec[..., self.n_filters // 2, :] /= np.sqrt(2)
-    #     return spec // (self.kernel_size // 2) ** 0.5
+    def pre_synthesis(self, spec):
+        spec = spec.clone()
+        spec[..., 0, :] /= np.sqrt(2)
+        spec[..., self.n_filters // 2, :] /= np.sqrt(2)
+        return spec / (self.kernel_size // 2) ** 0.5
 
 
 if __name__ == "__main__":
     from torch.testing import assert_allclose
 
-    wav = torch.randn(2, 1, 16000)
-    # for kernel_size in [16, 32, 64, 128, 256, 512]:
-    kernel_size = 256
-    stride = kernel_size // 2
+    wav = torch.randn(16000)
+    for kernel_size in [16, 32, 64, 128, 256, 512]:
+        stride = kernel_size // 2
 
-    window = torch.ones(kernel_size)  # / 2**0.5
+        window = torch.ones(kernel_size) / 2 ** 0.5
 
-    fb = TorchSTFT(
-        n_filters=kernel_size,
-        kernel_size=kernel_size,
-        stride=stride,
-        window=None,  # window.data.numpy()
-    )
-    window = torch.from_numpy(fb.window)
-    enc = Encoder(fb)
-    dec = Decoder(fb)
+        fb = TorchSTFT(
+            n_filters=kernel_size,
+            kernel_size=kernel_size,
+            stride=stride,
+            window=window.data.numpy(),
+        )
+        # window = torch.from_numpy(fb.window)
+        enc = Encoder(fb)
+        dec = Decoder(fb)
 
-    spec = torch.stft(
-        wav.squeeze(),
-        n_fft=kernel_size,
-        hop_length=stride,
-        win_length=kernel_size,
-        center=False,
-        window=window,
-    )
-    # wav_back = torch.istft(
-    #     spec,
-    #     n_fft=kernel_size,
-    #     hop_length=stride,
-    #     win_length=kernel_size,
-    #     window=window,
-    #     center=False,
-    # )
+        spec = torch.stft(
+            wav.squeeze(),
+            n_fft=kernel_size,
+            hop_length=stride,
+            win_length=kernel_size,
+            center=False,
+            window=window,
+        )
+        wav_back = torch.istft(
+            spec,
+            n_fft=kernel_size,
+            hop_length=stride,
+            win_length=kernel_size,
+            window=window,
+            center=False,
+            length=wav.shape[0],
+        )
 
-    spec = to_asteroid(spec.float())
-    asteroid_spec = enc(wav)
-    asteroid_wavback = dec(asteroid_spec)
+        spec = to_asteroid(spec.float())
+        asteroid_spec = enc(wav)
+        asteroid_wavback = dec(asteroid_spec)
 
-    assert_allclose(spec, asteroid_spec)
+        assert_allclose(spec, asteroid_spec)
+        assert_allclose(
+            wav_back[kernel_size:-kernel_size], asteroid_wavback[kernel_size:-kernel_size]
+        )
+
+    # plt.plot(wav.numpy(), "r")
+    # plt.plot(asteroid_wavback.numpy(), "b+")
+    # plt.plot(wav_back.numpy(), "k+")
+    # plt.show()
