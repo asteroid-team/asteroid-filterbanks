e0c9f39e93329378fbcb0d65db29e1470f7aff20
Cameron Maske <cameronmaske@gmail.com>
2021-04-07 22:14:16 +0200

    Spit PCEN into StatefulPCEN and PCEN. Add a StatefulPCEN.from_pcen class method to copy over. Updated tests to include checks against  parameter (and fixed that behaviour).

--- asteroid_filterbanks/pcen.py
+++ asteroid_filterbanks/pcen.py
@@ -29,27 +29,33 @@ class ExponentialMovingAverage(nn.Module):
     ):
         super().__init__()
         if per_channel:
-            self.weights = nn.Parameter(
-                torch.full((n_channels,), fill_value=smooth), requires_grad=trainable
-            )
+            weights = torch.full((n_channels,), fill_value=smooth)
         else:
-            self.weights = nn.Parameter(
-                torch.full((1,), fill_value=smooth), requires_grad=trainable
-            )
+            weights = torch.full((1,), fill_value=smooth)
+
+        self.set_weights(data=weights, trainable=trainable)
+
+    def set_weights(self, data: torch.Tensor, trainable: bool):
+        self.weights = nn.Parameter(data, requires_grad=trainable)
+        self.trainable = trainable
 
     def forward(
-        self, mag_spec: torch.Tensor, initial_state: torch.Tensor
+        self, mag_spec: torch.Tensor, initial_state: Optional[torch.Tensor]
     ) -> Tuple[torch.tensor, torch.Tensor]:
+        if initial_state is None:
+            initial_state = mag_spec[:, :, :, 0].unsqueeze(-1)
+
         weights = torch.clamp(self.weights, 0.0, 1.0)
-        accumulator = initial_state
+        accumulator = initial_state.transpose(1, -1)
         out = None
-        for x in torch.split(mag_spec, 1, dim=-1):
+        for x in torch.split(mag_spec.transpose(1, -1), 1, dim=1):
             accumulator = weights * x + (1.0 - weights) * accumulator
+            accumulator = accumulator
             if out is None:
-                out = accumulator
+                out = accumulator.transpose(1, -1)
             else:
-                out = torch.cat((out, accumulator), dim=-1)
-        return out, accumulator
+                out = torch.cat((out, accumulator.transpose(1, -1)), dim=-1)
+        return out, accumulator.transpose(1, -1)
 
 
 class TrainableParameters(TypedDict):
@@ -59,10 +65,136 @@ class TrainableParameters(TypedDict):
     smooth: bool
 
 
-class PCEN(nn.Module):
+class _PCEN(nn.Module):
+    def __init__(
+        self,
+        alpha: float = 0.96,
+        delta: float = 2.0,
+        root: float = 2.0,
+        floor: float = 1e-6,
+        smooth: float = 0.04,
+        n_channels: int = 1,
+        trainable: Union[bool, TrainableParameters] = False,
+        per_channel_smoothing: bool = False,
+    ):
+        super().__init__()
+
+        if trainable is True or trainable is False:
+            trainable = TrainableParameters(
+                alpha=trainable, delta=trainable, root=trainable, smooth=trainable
+            )
+
+        self.trainable = trainable
+        self.n_channels = n_channels
+
+        self.alpha = nn.Parameter(
+            torch.full((self.n_channels,), fill_value=alpha), requires_grad=self.trainable["alpha"]
+        )
+        self.delta = nn.Parameter(
+            torch.full((self.n_channels,), fill_value=delta), requires_grad=self.trainable["delta"]
+        )
+        self.root = nn.Parameter(
+            torch.full((self.n_channels,), fill_value=root), requires_grad=self.trainable["root"]
+        )
+
+        self.floor = floor
+        self.ema = ExponentialMovingAverage(
+            smooth=smooth,
+            per_channel=per_channel_smoothing,
+            n_channels=self.n_channels,
+            trainable=self.trainable["smooth"],
+        )
+
+    def forward(
+        self, mag_spec: torch.Tensor, initial_state: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        if len(mag_spec.shape) == 3:
+            # If n_channels is 1, add a single dimension to keep the shape consistent with multichannel shapes.
+            mag_spec = mag_spec.unsqueeze(1)
+
+        alpha = torch.min(self.alpha, torch.tensor(1.0))
+        root = torch.max(self.root, torch.tensor(1.0))
+        one_over_root = 1.0 / root
+        ema_smoother, hidden_state = self.ema(mag_spec, initial_state)
+        # Equation (1) in [1]
+        out = (
+            mag_spec.transpose(1, -1) / (self.floor + ema_smoother.transpose(1, -1)) ** alpha
+            + self.delta
+        ) ** one_over_root - self.delta ** one_over_root
+        return out.transpose(1, -1), hidden_state
+
+
+class PCEN(_PCEN):
     """Per-Channel Energy Normalization as described in [1].
 
-    This applies a fixed or learnable normalization by an exponential moving average smoother, and a compression.
+    This applies a fixed or learnable normalization by an exponential moving average smoother and a compression.
+
+    Args:
+        alpha: The AGC strength (or gain normalization strength) (α in the paper).
+            Defaults to 0.96
+        delta: Bias added before compression (δ in the paper).
+            Defaults to 2.0
+        root: One over exponent applied for compression (r in the paper).
+            Defaults to 2.0
+        floor: Offset added to compression, prevents division by zero. (ϵ in the paper)
+            Defaults to 1e-6
+        smooth: Smoothing coefficient (s in the paper).
+            Defaults to 0.04
+        n_channels: Number of channels in the time frequency representation.
+            Defaults to 1
+        trainable: If True, the parameters (alpha, delta, root and smooth) are trainable. If False, the parameters are fixed.
+            Individual parameters can set to be fixed or trainable by passing a dictionary of booleans, with the key
+            matching the parameter name and the value being either True (trainable) or False (fixed).
+            i.e. ``{"alpha": False, "delta": True, "root": False, "smooth": True}``
+            Defaults to False
+        per_channel_smoothing: If True, each channel has it's own smoothing coefficient.
+            Defaults to False
+
+    Examples
+
+        >>> audio = torch.randn(10, 2, 16_000)
+        >>> fb = STFTFB(kernel_size=256, n_filters=256, stride=128)
+        >>> enc = Encoder(fb)
+        >>> tf_rep = enc(audio)
+        >>> mag_spec = transform.mag(tf_rep)
+        >>> pcen = PCEN(n_channels=2)
+        >>> energy = pcen(mag_spec)
+        >>> # or alternative with a mel-spectrogram
+        >>> mel = MelScale(n_filters=256, sample_rate=16_000)
+        >>> mel_spec = mel(audio)
+        >>> energy = pcen(mag_spec)
+
+    References
+        [1]: Wang, Y., et al. "Trainable Frontend For Robust and Far-Field Keyword Spotting”, arXiv e-prints, 2016.
+             https://arxiv.org/pdf/1607.05666.pdf
+
+    Influenced by leaf-audio's tensorflow implementation
+        https://github.com/google-research/leaf-audio/blob/7ead2f9fe65da14c693c566fe8259ccaaf14129d/leaf_audio/postprocessing.py
+
+    See license here
+        https://github.com/google-research/leaf-audio/blob/master/LICENSE
+    """
+
+    def forward(self, mag_spec: torch.Tensor) -> torch.Tensor:
+        """Computes the PCEN from magnitude spectrum representation.
+
+        Args:
+            mag_spec: A tensor containing an magnitude spectrum representation.
+
+        Shapes
+            >>> (batch, n_channels, freq, time) -> (batch, n_channels, freq, time)
+            >>> (batch, freq, time) -> (batch, 1, freq, time) # Assumed to be mono-channel
+
+        """
+        return super().forward(mag_spec, None)[0]
+
+
+class StatefulPCEN(_PCEN):
+    """Per-Channel Energy Normalization as described in [1].
+
+    Suitable for continuous real-time processing of incoming audio.
+
+    This applies a fixed or learnable normalization by an exponential moving average smoother and a compression.
 
     Args:
         alpha: The AGC strength (or gain normalization strength) (α in the paper).
@@ -90,8 +222,11 @@ class PCEN(nn.Module):
         >>> fb = STFTFB(kernel_size=256, n_filters=256, stride=128)
         >>> enc = Encoder(fb)
         >>> tf_rep = enc(audio)
+        >>> mag_spec = transform.mag(tf_rep)
+        >>> mag_spec_1, mag_spec_2 = torch.chunk(mag_spec, 2, dim=-1)
         >>> pcen = PCEN(n_channels=2)
-        >>> energy = pcen(tf_rep)
+        >>> energy, hidden = pcen(mag_spec_1)
+        >>> energy, hidden = pcen(mag_spec_2, hidden)
 
     References
         [1]: Wang, Y., et al. "Trainable Frontend For Robust and Far-Field Keyword Spotting”, arXiv e-prints, 2016.
@@ -104,78 +239,32 @@ class PCEN(nn.Module):
         https://github.com/google-research/leaf-audio/blob/master/LICENSE
     """
 
-    def __init__(
-        self,
-        alpha: float = 0.96,
-        delta: float = 2.0,
-        root: float = 2.0,
-        floor: float = 1e-6,
-        smooth: float = 0.04,
-        n_channels: int = 1,
-        trainable: Union[bool, TrainableParameters] = False,
-        per_channel_smoothing: bool = False,
-    ):
-        super().__init__()
-
-        if trainable is True or trainable is False:
-            trainable = TrainableParameters(
-                alpha=trainable, delta=trainable, root=trainable, smooth=trainable
-            )
-
-        self.alpha = nn.Parameter(
-            torch.full((n_channels,), fill_value=alpha), requires_grad=trainable["alpha"]
-        )
-        self.delta = nn.Parameter(
-            torch.full((n_channels,), fill_value=delta), requires_grad=trainable["delta"]
-        )
-        self.root = nn.Parameter(
-            torch.full((n_channels,), fill_value=root), requires_grad=trainable["root"]
-        )
-
-        self.floor = floor
-        self.ema = ExponentialMovingAverage(
-            smooth=smooth,
-            per_channel=per_channel_smoothing,
-            n_channels=n_channels,
-            trainable=trainable["smooth"],
-        )
+    @classmethod
+    def from_pcen(cls, pcen: PCEN):
+        stateful = cls()
+        stateful.alpha = pcen.alpha
+        stateful.delta = pcen.delta
+        stateful.root = pcen.root
+        stateful.floor = pcen.floor
+        stateful.ema.set_weights(pcen.ema.weights.data, pcen.ema.trainable)
+        return stateful
 
     def forward(
-        self, tf_rep: torch.Tensor, initial_state: Optional[torch.Tensor] = None
-    ) -> Tuple[torch.tensor, torch.Tensor]:
-        """Computes the PCEN from a complex frequency representation.
+        self, mag_spec: torch.Tensor, initial_state: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Computes the PCEN from magnitude spectrum representation, and an optional smoothed version of the filterbank (Equation (2) in [1]).
 
         Args:
-            tf_rep: A tensor containing an asteroid complex time frequency representation.
+            mag_spec: tensor containing an magnitude spectrum representation.
             initial_state: A tensor containing the initial hidden state.
-                If set to None, defaults to the first element by time (dim=-1) in the tf_rep tensor.
+                If set to None, defaults to the first element by time (dim=-1) in the mag_spec tensor.
                 Defaults to None
-
         Shapes
-            >>> (batch, n_channels, freq, time), None -> (batch, n_channels, freq // 2 + 1, time), (batch_size, n_channel, freq // 2 + 1, 1)
-            >>> (batch, n_channels, freq, time), (batch_size, n_channel, freq // 2 + 1, 1) -> (batch, n_channels, freq // 2 + 1, time), (batch_size, n_channel, freq // 2 + 1, 1)
+            >>> (batch, n_channels, freq, time) -> (batch, n_channels, freq, time), (batch_size, n_channel, freq, 1)
+            >>> (batch, freq, time) -> (batch, 1, freq, time), , (batch_size, n_channel, freq, 1) # Assumed to be mono-channel
 
+        References
+            [1]: Wang, Y., et al. "Trainable Frontend For Robust and Far-Field Keyword Spotting”, arXiv e-prints, 2016.
+                https://arxiv.org/pdf/1607.05666.pdf
         """
-        if not transforms.is_asteroid_complex(tf_rep):
-            raise AssertionError(
-                f"Expected a complex-like tensor of shape (batch, n_channels, freq, time)."
-            )
-        mag_spec = transforms.mag(tf_rep, dim=-2)
-
-        if len(tf_rep.shape) == 3:
-            # If n_channels is 1, add a single dimension to keep the shape consistent with multichannel shapes.
-            mag_spec = mag_spec.unsqueeze(1)
-
-        if initial_state is None:
-            initial_state = mag_spec[:, :, :, 0].unsqueeze(-1)
-
-        alpha = torch.min(self.alpha, torch.tensor(1.0))
-        root = torch.max(self.root, torch.tensor(1.0))
-        one_over_root = 1.0 / root
-        ema_smoother, hidden_state = self.ema(mag_spec, initial_state=initial_state)
-        # Equation (1) in [1]
-        out = (
-            mag_spec.transpose(1, -1) / (self.floor + ema_smoother.transpose(1, -1)) ** alpha
-            + self.delta
-        ) ** one_over_root - self.delta ** one_over_root
-        return out.transpose(1, -1), hidden_state
+        return super().forward(mag_spec, initial_state)
--- tests/pcen_test.py
+++ tests/pcen_test.py
@@ -1,20 +1,22 @@
 import torch
-from asteroid_filterbanks import Encoder, Decoder, STFTFB
-from asteroid_filterbanks.pcen import PCEN, ExponentialMovingAverage
+from asteroid_filterbanks import Encoder, Decoder, STFTFB, transforms
+from asteroid_filterbanks.pcen import PCEN, StatefulPCEN, ExponentialMovingAverage
 from torch.testing import assert_allclose
 import pytest
 import re
 
 
-@pytest.mark.parametrize("n_channels", [1, 2, 4])
+@pytest.mark.parametrize("n_channels", [2, 4])
 @pytest.mark.parametrize("batch_size", [1, 10])
 @pytest.mark.parametrize("n_filters", [128, 512])
 @pytest.mark.parametrize("timesteps", [1, 10])
-def test_ema(n_channels, batch_size, n_filters, timesteps):
-    ema = ExponentialMovingAverage(n_channels=n_channels)
-
+@pytest.mark.parametrize("per_channel", [True, False])
+@pytest.mark.parametrize("initial_state", [None, True])
+def test_ema(n_channels, batch_size, n_filters, timesteps, per_channel, initial_state):
+    ema = ExponentialMovingAverage(n_channels=n_channels, per_channel=True)
     mag_spec = torch.randn(batch_size, n_channels, n_filters, timesteps)
-    initial_state = mag_spec[:, :, :, 0:1]
+    if initial_state:
+        initial_state = mag_spec[:, :, :, 0:1]
 
     out, hidden = ema(mag_spec, initial_state)
     assert out.shape == (batch_size, n_channels, n_filters, timesteps)
@@ -50,36 +52,75 @@ def test_pcen_init():
 
 @pytest.mark.parametrize("n_channels", [1, 2, 4])
 @pytest.mark.parametrize("batch_size", [1, 10])
-def test_pcen(n_channels, batch_size):
+def test_pcen_forward(n_channels, batch_size):
     audio = torch.randn(batch_size, n_channels, 16000 * 10)
 
     fb = STFTFB(kernel_size=256, n_filters=256, stride=128)
     enc = Encoder(fb)
     tf_rep = enc(audio)
+    mag_spec = transforms.mag(tf_rep)
 
     pcen = PCEN(n_channels=n_channels)
-    energy, hidden = pcen(tf_rep)
+    energy = pcen(mag_spec)
 
     assert energy.shape == (batch_size, n_channels, 256 // 2 + 1, 1249)
-    assert hidden.shape == (batch_size, n_channels, 256 // 2 + 1, 1)
+
+
+@pytest.mark.parametrize("n_channels", [1, 2, 4])
+@pytest.mark.parametrize("batch_size", [1, 10])
+@pytest.mark.parametrize("n_filters", [128, 512])
+@pytest.mark.parametrize("timesteps", [1, 10])
+def test_pcen_trainable(n_channels, batch_size, n_filters, timesteps):
+    mag_spec = torch.randn((batch_size, n_channels, n_filters, timesteps))
+
+    pcen = PCEN(trainable=True, n_channels=n_channels)
+    energy_rep = pcen(mag_spec)
+
+    energy_rep.mean().backward()
+
+
+def test_stateful_pcen_init():
+    pcen = StatefulPCEN(
+        alpha=0.0,
+        delta=0.5,
+        root=1.0,
+        floor=1.5,
+        smooth=2.0,
+        n_channels=2,
+        per_channel_smoothing=True,
+        trainable={"alpha": True, "delta": False, "root": True, "smooth": False},
+    )
+
+    assert_allclose(pcen.alpha.data, torch.tensor([0.0, 0.0]))
+    assert pcen.alpha.requires_grad is True
+
+    assert_allclose(pcen.delta.data, torch.tensor([0.5, 0.5]))
+    assert pcen.delta.requires_grad is False
+
+    assert_allclose(pcen.root.data, torch.tensor([1.0, 1.0]))
+    assert pcen.root.requires_grad is True
+
+    assert pcen.floor == 1.5
+
+    assert_allclose(pcen.ema.weights.data, torch.tensor([2.0, 2.0]))
+    assert pcen.ema.weights.requires_grad is False
 
 
 @pytest.mark.parametrize("n_channels", [1, 2, 4])
 @pytest.mark.parametrize("batch_size", [1, 10])
 @pytest.mark.parametrize("n_filters", [128, 512])
 @pytest.mark.parametrize("timesteps", [3, 10])
-def test_pcen_hidden_state(n_channels, batch_size, n_filters, timesteps):
-    pcen = PCEN(n_channels=n_channels)
+def test_stateful_pcen_forward(n_channels, batch_size, n_filters, timesteps):
+    pcen = StatefulPCEN(n_channels=n_channels)
 
-    tf_rep = torch.randn((batch_size, n_channels, n_filters, timesteps))
+    mag_spec = torch.randn((batch_size, n_channels, n_filters, timesteps))
+    mag_spec_1, mag_spec_2 = torch.chunk(mag_spec, 2, dim=-1)
 
-    tf_rep_1, tf_rep_2 = torch.chunk(tf_rep, 2, dim=-1)
-
-    energy_1, hidden = pcen(tf_rep_1)
-    energy_2, hidden = pcen(tf_rep_2, hidden)
+    energy_1, hidden = pcen(mag_spec_1)
+    energy_2, hidden = pcen(mag_spec_2, hidden)
     energy = torch.cat((energy_1, energy_2), dim=-1)
 
-    expected_energy, expected_hidden = pcen(tf_rep)
+    expected_energy, expected_hidden = pcen(mag_spec)
     assert_allclose(expected_energy, energy)
     assert_allclose(expected_hidden, hidden)
 
@@ -88,23 +129,15 @@ def test_pcen_hidden_state(n_channels, batch_size, n_filters, timesteps):
 @pytest.mark.parametrize("batch_size", [1, 10])
 @pytest.mark.parametrize("n_filters", [128, 512])
 @pytest.mark.parametrize("timesteps", [1, 10])
-def test_pcen_trainable(n_channels, batch_size, n_filters, timesteps):
-    tf_rep = torch.randn((batch_size, n_channels, n_filters, timesteps))
-
-    pcen = PCEN(trainable=True, n_channels=n_channels)
-    energy_rep, _ = pcen(tf_rep)
-
-    energy_rep.mean().backward()
-
-
-def test_pcen_forward_shape_is_asteroid_complex():
-    pcen = PCEN(trainable=True, n_channels=2)
-
-    with pytest.raises(
-        AssertionError,
-        match=re.escape("Expected a complex-like tensor of shape (batch, n_channels, freq, time)."),
-    ):
-        pcen(torch.randn(10, 1, 1, 1))
+@pytest.mark.parametrize("trainable", [True, False])
+def test_pcen_jit(n_channels, batch_size, n_filters, timesteps, trainable):
+    mag_spec = torch.randn((batch_size, n_channels, n_filters, timesteps))
+    pcen = PCEN(trainable=trainable, n_channels=n_channels)
+    traced = torch.jit.trace(pcen, mag_spec)
+    with torch.no_grad():
+        native_energy = pcen(mag_spec)
+        jit_energy = traced(mag_spec)
+        assert_allclose(native_energy, jit_energy)
 
 
 @pytest.mark.parametrize("n_channels", [1, 2, 4])
@@ -112,12 +145,43 @@ def test_pcen_forward_shape_is_asteroid_complex():
 @pytest.mark.parametrize("n_filters", [128, 512])
 @pytest.mark.parametrize("timesteps", [1, 10])
 @pytest.mark.parametrize("trainable", [True, False])
-def test_pcen_jit(n_channels, batch_size, n_filters, timesteps, trainable):
-    tf_rep = torch.randn((batch_size, n_channels, n_filters, timesteps))
-    pcen = PCEN(trainable=trainable, n_channels=n_channels)
-    traced = torch.jit.trace(pcen, tf_rep)
+def test_stateful_pcen_jit(n_channels, batch_size, n_filters, timesteps, trainable):
+    mag_spec = torch.randn((batch_size, n_channels, n_filters, timesteps))
+    pcen = StatefulPCEN(trainable=trainable, n_channels=n_channels)
+    traced = torch.jit.trace(pcen, mag_spec)
     with torch.no_grad():
-        native_out, native_hidden = pcen(tf_rep)
-        jit_out, jit_hidden = traced(tf_rep)
+        native_out, native_hidden = pcen(mag_spec)
+        jit_out, jit_hidden = traced(mag_spec)
         assert_allclose(native_out, jit_out)
         assert_allclose(native_hidden, jit_hidden)
+
+
+@pytest.mark.parametrize("n_channels", [1, 2, 4])
+@pytest.mark.parametrize("batch_size", [1, 10])
+@pytest.mark.parametrize("n_filters", [128, 512])
+@pytest.mark.parametrize("timesteps", [3, 10])
+@pytest.mark.parametrize("per_channel_smoothing", [True, False])
+@pytest.mark.parametrize("trainable", [True, False])
+def test_stateful_pcen_from_pcen(
+    n_channels, batch_size, n_filters, timesteps, per_channel_smoothing, trainable
+):
+    mag_spec = torch.randn(batch_size, n_channels, n_filters, timesteps)
+
+    pcen = PCEN(
+        alpha=0.0,
+        delta=0.5,
+        root=1.0,
+        floor=1.5,
+        smooth=2.0,
+        n_channels=n_channels,
+        per_channel_smoothing=per_channel_smoothing,
+        trainable=trainable,
+    )
+
+    stateful_pcen = StatefulPCEN.from_pcen(pcen)
+
+    expected_energy = pcen(mag_spec)
+    energy, hidden = stateful_pcen(mag_spec)
+
+    assert_allclose(expected_energy, energy)
+    assert hidden.shape == (batch_size, n_channels, n_filters, 1)
